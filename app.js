14219a9aa13eee5a958c20e73c35179d815b9f32
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-06-11 13:54:55 +0900

    #2.1 2D Context

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,22 +10,29 @@ const stopPainting = () => {
   painting = false;
 };
 
+const startPainting = () => {
+  painting = true;
+};
+
 const onMouseMove = (event) => {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 };
 
 const onMouseDown = (event) => {
   painting = true;
 };
 
-const onMouseUp = (event) => {
-  stopPainting();
-};
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
